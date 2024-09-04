ba21f2b2bdbf80ae4d712f762b4b0711fdfaed15
fatinsomniac <narantikadwi16@gmail.com>
2024-09-04 09:46:36 +0700

    navbar change

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,11 +1,11 @@
-<nav class="navbar navbar-expand-lg bg-primary">
+<nav class="navbar navbar-expand-lg bg-warning">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Medium</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link" href="{{ route('ourstory') }}">Our Story</a>
         </li>
@@ -17,6 +17,9 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{ route('signin') }}">Sign In</a>
+        </li>
+        <li class="nav-item">
+          <a class="btn btn-light rounded-pill" href="#">Get Started</a>
         </li>
       </ul>
     </div>
