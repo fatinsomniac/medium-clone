768ec8aa9323641f1bdcc6aaf6a73aa2ce2a059f
fatinsomniac <narantikadwi16@gmail.com>
2024-09-04 10:28:44 +0700

    change navbar bg-color

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-warning">
+<nav class="navbar navbar-expand-lg bg-light" style="border: 1px solid black;">
   <div class="container-fluid">
     <a class="navbar-brand ms-5" href="#">Medium</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,19 +7,19 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
         <li class="nav-item me-3">
-          <a class="nav-link" href="{{ route('ourstory') }}">Our Story</a>
+          <a class="nav-link {{ (request()->is('/*')) ? 'active' : '' }}" href="{{ route('ourstory') }}">Our Story</a>
         </li>
         <li class="nav-item me-3">
-          <a class="nav-link" href="{{ route('membership') }}">Membership</a>
+          <a class="nav-link {{ (request()->is('membership*')) ? 'active' : '' }}" href="{{ route('membership') }}">Membership</a>
         </li>
         <li class="nav-item me-3">
-          <a class="nav-link" href="{{ route('write') }}">Write</a>
+          <a class="nav-link {{ (request()->is('write*')) ? 'active' : '' }}" href="{{ route('write') }}">Write</a>
         </li>
         <li class="nav-item me-3">
-          <a class="nav-link" href="{{ route('signin') }}">Sign In</a>
+          <a class="nav-link {{ (request()->is('signin*')) ? 'active' : '' }}" href="{{ route('signin') }}">Sign In</a>
         </li>
         <li class="nav-item me-3">
-          <a class="btn btn-light rounded-pill" href="#">Get Started</a>
+          <a class="btn btn-light rounded-pill bg-dark" style="color: #fff" href="#">Get Started</a>
         </li>
       </ul>
     </div>
